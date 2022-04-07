0dfbd93b29a068c753fccebb2e5969f77b3b427c
Ben West <bewest@gmail.com>
2022-04-07 10:43:48 -0700

    stub out initial routes

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var express = require('express')
   , favicon = require('serve-favicon')
   , logger = require('morgan')
   , config = require('./env')
+  , passport = require('passport')
+  , session = require('express-session')
   ;
 
 var app = express();
@@ -25,12 +27,38 @@ app.set('query parser', 'extended');
 app.use(require('stylus').middleware(config.express.stylus_path));
 app.use(express.static(config.express.static_path));
 
+
+var session_config = {
+  secret: config.express.session_secret,
+  resave: false,
+  saveUnitialized: true,
+  cookie: { }
+};
+
 if (app.get('env') == 'development') {
 	app.locals.pretty = true;
 }
+
 app.locals.software = config.software;
 
+if (app.get('env') == 'production') {
+  app.set('trust proxy', 1);
+  session_config.cookie.secure == true;
+}
+
+app.use(session(session_config));
+
+
 app.get('/', routes.index);
+
+app.get('/dashboard', routes.index);
+app.get('/auth/connect', routes.index);
+app.get('/auth/logout', routes.index);
+app.get('/auth/connect/t1pal', routes.index);
+app.get('/api/sites', routes.index);
+app.get('/api/sites/:site_name', routes.index);
+app.get('/api/sites/:site_name/views', routes.index);
+app.get('/api/sites/:site_name/views/:view_name', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
